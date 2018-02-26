02af5d41b49feabf7937454494d69005821992b4
Michal <228007@student.pwr.edu.pl>
2018-02-26 23:17:35 +0100

    Samo menu jest, ale jest problem z przechodzeniem pomiędzy ekranami. Aktualnie umieściłem tam fragmenty, ale jest jakiś problem podczas wywoływania funkcji "onAttach" w danym fragmencie. Powróce do tematu niedługo.

--- app/src/main/java/wojslawice/arboretum/MainActivity.java
+++ app/src/main/java/wojslawice/arboretum/MainActivity.java
@@ -1,17 +1,124 @@
 package wojslawice.arboretum;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * This is just a test comment to check GitHub working with Android Studio
-     */
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        //mDrawerLayout.closeDrawers();
+
+                        selectDrawerItem(menuItem);
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        // actions during menu changes
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+    }
+
+    public void selectDrawerItem(MenuItem menuItem){
+
+
+        switch(menuItem.getItemId())
+        {
+            case R.id.nav_menu:
+
+
+                MainFragment mainFragment = new MainFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame,mainFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+                break;
+
+            case R.id.nav_route:
+                MainFragment routeFragment = new MainFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame, routeFragment)
+                        .addToBackStack(null)
+                        .commit();
+                break;
+
+            default:
+                MainFragment defaultFragment = new MainFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame,defaultFragment)
+                        .addToBackStack(null)
+                        .commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
